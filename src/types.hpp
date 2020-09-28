d5a1dba7d1ffd4de95cefd870092166f87c9cc07
cdde <3018231-cdde@users.noreply.gitlab.com>
2020-09-28 17:29:53 -0500

    Deleteding unused files

---
+++ src/types.hpp
@@ -0,0 +1,26 @@
+/*
+ * This file is part of Quick Run and the Melon engine
+ * Copyright (C) 2020 Decent Games. Some or all rights reserved.
+ */
+
+struct vec3d {
+    float x;
+    float y;
+    float z;
+};
+
+struct vec3dd {
+    double x;
+    double y;
+    double z;
+};
+
+struct vec3dm {
+    float x;
+    float y;
+    float z;
+};
+
+struct State {
+    vec3d pos;
+};
